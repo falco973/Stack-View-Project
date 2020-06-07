680a99c3fe30909edcc79dc38ce2cd6716057f3d
Annamaria <annamaria.ferrante1956@gmail.com>
2020-06-07 16:04:05 +0200

    Initial Commit

---
+++ Stack View Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stack View Project
+//
+//  Created by Gennaro Ferrante on 07/06/2020.
+//  Copyright © 2020 Gennaro Ferrante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
